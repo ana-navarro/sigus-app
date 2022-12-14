372897e60e3ea7b3745149e9d2070517e02b1c4d
Ana Navarro <ana.elisacn.98@gmail.com>
2022-12-13 21:23:53 -0300

    fixing error 405

--- src/services/api.js
+++ src/services/api.js
@@ -6,9 +6,9 @@ const api = axios.create({
 
 api.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
 api.defaults.headers.common['Access-Control-Allow-Credentials'] = 'true'
-api.defaults.headers.common['Access-Control-Allow-Headers'] = 'Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization'
+api.defaults.headers.common['Access-Control-Allow-Headers'] = '*'
 api.defaults.headers.common['Content'] = 'application/json'
-api.defaults.headers.common['Access-Control-Allow-Methods'] = 'POST, PUT, GET, DELETE'
+api.defaults.headers.common['Access-Control-Allow-Methods'] = '*'
 api.defaults.headers.common['ngrok-skip-browser-warning'] = 'any'
 
 export default api
