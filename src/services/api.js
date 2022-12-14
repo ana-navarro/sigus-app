73cb3b9b752d6107f9638dc8e5efe6f00445bb01
Ana Navarro <ana.elisacn.98@gmail.com>
2022-12-14 16:44:39 -0300

    Content-type

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://181.215.134.184:5000'
+    baseUrl: 'http://181.215.134.184:5000'
 });
 
 api.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
 api.defaults.headers.common['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-CSRF-TOKEN, Game-Token, developerKey'
-api.defaults.headers.common['Content'] = 'application/json'
+api.defaults.headers.common['Content-Type'] = 'application/json'
 api.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
 api.defaults.headers.common['ngrok-skip-browser-warning'] = 'any'
 
