7d41c10a92052268f9ebd07e0efc79975ab68e35
Ana Navarro <ana.elisacn.98@gmail.com>
2022-12-13 21:31:44 -0300

    fixing error 405

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseUrl: 'http://181.215.134.184:5000/'
+    baseUrl: 'http://181.215.134.184:5000'
 });
 
 api.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
-api.defaults.headers.common['Access-Control-Allow-Credentials'] = 'true'
-api.defaults.headers.common['Access-Control-Allow-Headers'] = '*'
+api.defaults.headers.common['Access-Control-Allow-Credentials'] = 'false'
+api.defaults.headers.common['Access-Control-Allow-Headers'] = "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, x-auth-token"
 api.defaults.headers.common['Content'] = 'application/json'
-api.defaults.headers.common['Access-Control-Allow-Methods'] = '*'
+api.defaults.headers.common['Access-Control-Allow-Methods'] = "GET,OPTIONS,PATCH,DELETE,POST,PUT"
 api.defaults.headers.common['ngrok-skip-browser-warning'] = 'any'
 
 export default api
